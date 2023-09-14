669e035a607db962343dff3c3aebd08ddbefda4d
deaththekidd <ohiemidivine7@gmail.com>
2023-09-14 04:36:29 +0100

    update

--- public/scripts/loader.js
+++ public/scripts/loader.js
@@ -22,149 +22,159 @@ window.addEventListener("load", function () {
 		reset: false,
 	};
 
-	if (settingsObject.reset) {
-		localStorage.clear();
-	}
+	try {
+		if (settingsObject.reset) {
+			localStorage.clear();
+		}
 
-	if (localStorage.length === 0) {
-		corkThemeObject = settingsObject;
-	} else {
-		getcorkThemeObject = localStorage.getItem("theme");
-		getParseObject = JSON.parse(getcorkThemeObject);
-		ParsedObject = getParseObject;
+		if (localStorage.length === 0) {
+			corkThemeObject = settingsObject;
+		} else {
+			getcorkThemeObject = localStorage.getItem("theme");
+			getParseObject = JSON.parse(getcorkThemeObject);
+			ParsedObject = getParseObject;
 
-		if (getcorkThemeObject !== null) {
-			if (ParsedObject.admin === "Exiat Frontend") {
-				if (ParsedObject.settings.layout.name === layoutName) {
-					corkThemeObject = ParsedObject;
+			if (getcorkThemeObject !== null) {
+				if (ParsedObject.admin === "Exiat Frontend") {
+					if (ParsedObject.settings.layout.name === layoutName) {
+						corkThemeObject = ParsedObject;
+					} else {
+						corkThemeObject = settingsObject;
+					}
 				} else {
-					corkThemeObject = settingsObject;
+					if (ParsedObject.admin === undefined) {
+						corkThemeObject = settingsObject;
+					}
 				}
 			} else {
-				if (ParsedObject.admin === undefined) {
-					corkThemeObject = settingsObject;
+				corkThemeObject = settingsObject;
+			}
+		}
+
+		// Get Dark Mode Information i.e darkMode: true or false
+
+		if (corkThemeObject?.settings.layout.darkMode) {
+			localStorage.setItem("theme", JSON.stringify(corkThemeObject));
+			getcorkThemeObject = localStorage.getItem("theme");
+			getParseObject = JSON.parse(getcorkThemeObject);
+
+			if (getParseObject.settings.layout.darkMode) {
+				let ifStarterKit =
+					document.body.getAttribute("page") === "starter-pack" ? true : false;
+				document.body.classList.add("dark");
+				if (ifStarterKit) {
+					if (document.querySelector(".navbar-logo")) {
+						document
+							.querySelector(".navbar-logo")
+							.setAttribute("src", "/logo.svg");
+					}
+				} else {
+					if (document.querySelector(".navbar-logo")) {
+						document
+							.querySelector(".navbar-logo")
+							.setAttribute(
+								"src",
+								getParseObject.settings.layout.logo.darkLogo
+							);
+					}
 				}
 			}
 		} else {
-			corkThemeObject = settingsObject;
-		}
-	}
+			localStorage.setItem("theme", JSON.stringify(corkThemeObject));
+			getcorkThemeObject = localStorage.getItem("theme");
+			getParseObject = JSON.parse(getcorkThemeObject);
 
-	// Get Dark Mode Information i.e darkMode: true or false
-
-	if (corkThemeObject?.settings.layout.darkMode) {
-		localStorage.setItem("theme", JSON.stringify(corkThemeObject));
-		getcorkThemeObject = localStorage.getItem("theme");
-		getParseObject = JSON.parse(getcorkThemeObject);
-
-		if (getParseObject.settings.layout.darkMode) {
-			let ifStarterKit =
-				document.body.getAttribute("page") === "starter-pack" ? true : false;
-			document.body.classList.add("dark");
-			if (ifStarterKit) {
-				if (document.querySelector(".navbar-logo")) {
-					document
-						.querySelector(".navbar-logo")
-						.setAttribute("src", "/logo.svg");
-				}
-			} else {
-				if (document.querySelector(".navbar-logo")) {
-					document
-						.querySelector(".navbar-logo")
-						.setAttribute("src", getParseObject.settings.layout.logo.darkLogo);
+			if (!getParseObject.settings.layout.darkMode) {
+				let ifStarterKit =
+					document.body.getAttribute("page") === "starter-pack" ? true : false;
+				document.body.classList.remove("dark");
+				if (ifStarterKit) {
+					if (document.querySelector(".navbar-logo")) {
+						document
+							.querySelector(".navbar-logo")
+							.setAttribute("src", "/logo2.svg");
+					}
+				} else {
+					if (document.querySelector(".navbar-logo")) {
+						document
+							.querySelector(".navbar-logo")
+							.setAttribute(
+								"src",
+								getParseObject.settings.layout.logo.lightLogo
+							);
+					}
 				}
 			}
 		}
-	} else {
-		localStorage.setItem("theme", JSON.stringify(corkThemeObject));
-		getcorkThemeObject = localStorage.getItem("theme");
-		getParseObject = JSON.parse(getcorkThemeObject);
 
-		if (!getParseObject.settings.layout.darkMode) {
-			let ifStarterKit =
-				document.body.getAttribute("page") === "starter-pack" ? true : false;
-			document.body.classList.remove("dark");
-			if (ifStarterKit) {
-				if (document.querySelector(".navbar-logo")) {
-					document
-						.querySelector(".navbar-logo")
-						.setAttribute("src", "/logo2.svg");
+		// Get Layout Information i.e boxed: true or false
+
+		if (corkThemeObject.settings.layout.boxed) {
+			localStorage.setItem("theme", JSON.stringify(corkThemeObject));
+			getcorkThemeObject = localStorage.getItem("theme");
+			getParseObject = JSON.parse(getcorkThemeObject);
+
+			if (getParseObject.settings.layout.boxed) {
+				if (document.body.getAttribute("layout") !== "full-width") {
+					document.body.classList.add("layout-boxed");
+					if (document.querySelector(".header-container")) {
+						document
+							.querySelector(".header-container")
+							.classList.add("container-xxl");
+					}
+					if (document.querySelector(".middle-content")) {
+						document
+							.querySelector(".middle-content")
+							.classList.add("container-xxl");
+					}
+				} else {
+					document.body.classList.remove("layout-boxed");
+					if (document.querySelector(".header-container")) {
+						document
+							.querySelector(".header-container")
+							.classList.remove("container-xxl");
+					}
+					if (document.querySelector(".middle-content")) {
+						document
+							.querySelector(".middle-content")
+							.classList.remove("container-xxl");
+					}
 				}
-			} else {
-				if (document.querySelector(".navbar-logo")) {
-					document
-						.querySelector(".navbar-logo")
-						.setAttribute("src", getParseObject.settings.layout.logo.lightLogo);
+			}
+		} else {
+			localStorage.setItem("theme", JSON.stringify(corkThemeObject));
+			getcorkThemeObject = localStorage.getItem("theme");
+			getParseObject = JSON.parse(getcorkThemeObject);
+
+			if (!getParseObject.settings.layout.boxed) {
+				if (document.body.getAttribute("layout") !== "boxed") {
+					document.body.classList.remove("layout-boxed");
+					if (document.querySelector(".header-container")) {
+						document
+							.querySelector(".header-container")
+							.classList.remove("container-xxl");
+					}
+					if (document.querySelector(".middle-content")) {
+						document
+							.querySelector(".middle-content")
+							.classList.remove("container-xxl");
+					}
+				} else {
+					document.body.classList.add("layout-boxed");
+					if (document.querySelector(".header-container")) {
+						document
+							.querySelector(".header-container")
+							.classList.add("container-xxl");
+					}
+					if (document.querySelector(".middle-content")) {
+						document
+							.querySelector(".middle-content")
+							.classList.add("container-xxl");
+					}
 				}
 			}
 		}
-	}
-
-	// Get Layout Information i.e boxed: true or false
-
-	if (corkThemeObject.settings.layout.boxed) {
-		localStorage.setItem("theme", JSON.stringify(corkThemeObject));
-		getcorkThemeObject = localStorage.getItem("theme");
-		getParseObject = JSON.parse(getcorkThemeObject);
-
-		if (getParseObject.settings.layout.boxed) {
-			if (document.body.getAttribute("layout") !== "full-width") {
-				document.body.classList.add("layout-boxed");
-				if (document.querySelector(".header-container")) {
-					document
-						.querySelector(".header-container")
-						.classList.add("container-xxl");
-				}
-				if (document.querySelector(".middle-content")) {
-					document
-						.querySelector(".middle-content")
-						.classList.add("container-xxl");
-				}
-			} else {
-				document.body.classList.remove("layout-boxed");
-				if (document.querySelector(".header-container")) {
-					document
-						.querySelector(".header-container")
-						.classList.remove("container-xxl");
-				}
-				if (document.querySelector(".middle-content")) {
-					document
-						.querySelector(".middle-content")
-						.classList.remove("container-xxl");
-				}
-			}
-		}
-	} else {
-		localStorage.setItem("theme", JSON.stringify(corkThemeObject));
-		getcorkThemeObject = localStorage.getItem("theme");
-		getParseObject = JSON.parse(getcorkThemeObject);
-
-		if (!getParseObject.settings.layout.boxed) {
-			if (document.body.getAttribute("layout") !== "boxed") {
-				document.body.classList.remove("layout-boxed");
-				if (document.querySelector(".header-container")) {
-					document
-						.querySelector(".header-container")
-						.classList.remove("container-xxl");
-				}
-				if (document.querySelector(".middle-content")) {
-					document
-						.querySelector(".middle-content")
-						.classList.remove("container-xxl");
-				}
-			} else {
-				document.body.classList.add("layout-boxed");
-				if (document.querySelector(".header-container")) {
-					document
-						.querySelector(".header-container")
-						.classList.add("container-xxl");
-				}
-				if (document.querySelector(".middle-content")) {
-					document
-						.querySelector(".middle-content")
-						.classList.add("container-xxl");
-				}
-			}
-		}
+	} catch (error) {
+		console.log(error);
 	}
 });
